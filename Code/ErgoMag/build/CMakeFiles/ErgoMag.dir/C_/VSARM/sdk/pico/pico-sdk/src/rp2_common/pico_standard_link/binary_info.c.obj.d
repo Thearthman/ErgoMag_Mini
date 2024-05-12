afbd1e489e8a09da0033fa0caea36b9397895ee9
John Mich <wenruishan1@gmail.com>
2024-05-12 12:19:23 +0800

    First Successful Test Firmware

---
+++ Code/ErgoMag/build/CMakeFiles/ErgoMag.dir/C_/VSARM/sdk/pico/pico-sdk/src/rp2_common/pico_standard_link/binary_info.c.obj.d
@@ -0,0 +1,36 @@
+CMakeFiles/ErgoMag.dir/C_/VSARM/sdk/pico/pico-sdk/src/rp2_common/pico_standard_link/binary_info.c.obj: \
+ C:\VSARM\sdk\pico\pico-sdk\src\rp2_common\pico_standard_link\binary_info.c \
+ C:\VSARM\sdk\pico\pico-sdk\src\common\pico_binary_info\include/pico/binary_info.h \
+ C:\VSARM\sdk\pico\pico-sdk\src\common\pico_binary_info\include/pico/binary_info/defs.h \
+ C:\VSARM\sdk\pico\pico-sdk\src\common\pico_binary_info\include/pico/binary_info/structure.h \
+ C:/VSARM/armcc/13C7F1~1.2RE/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h \
+ C:/VSARM/armcc/13C7F1~1.2RE/arm-none-eabi/include/stdint.h \
+ C:/VSARM/armcc/13C7F1~1.2RE/arm-none-eabi/include/machine/_default_types.h \
+ C:/VSARM/armcc/13C7F1~1.2RE/arm-none-eabi/include/sys/features.h \
+ C:/VSARM/armcc/13C7F1~1.2RE/arm-none-eabi/include/_newlib_version.h \
+ C:/VSARM/armcc/13C7F1~1.2RE/arm-none-eabi/include/sys/_intsup.h \
+ C:/VSARM/armcc/13C7F1~1.2RE/arm-none-eabi/include/sys/_stdint.h \
+ C:\VSARM\sdk\pico\pico-sdk\src\common\pico_binary_info\include/pico/binary_info/code.h \
+ C:\VSARM\sdk\pico\pico-sdk\src\common\pico_base\include/pico.h \
+ C:\VSARM\sdk\pico\pico-sdk\src\common\pico_base\include/pico/types.h \
+ C:\VSARM\sdk\pico\pico-sdk\src\common\pico_base\include/pico/assert.h \
+ C:/VSARM/armcc/13C7F1~1.2RE/lib/gcc/arm-none-eabi/13.2.1/include/stdbool.h \
+ C:/VSARM/armcc/13C7F1~1.2RE/arm-none-eabi/include/assert.h \
+ C:/VSARM/armcc/13C7F1~1.2RE/arm-none-eabi/include/_ansi.h \
+ C:/VSARM/armcc/13C7F1~1.2RE/arm-none-eabi/include/newlib.h \
+ C:/VSARM/armcc/13C7F1~1.2RE/arm-none-eabi/include/sys/config.h \
+ C:/VSARM/armcc/13C7F1~1.2RE/arm-none-eabi/include/machine/ieeefp.h \
+ C:/VSARM/armcc/13C7F1~1.2RE/lib/gcc/arm-none-eabi/13.2.1/include/stddef.h \
+ L:\Work_and_Homework\Work\Personal\ErgoMag\ErgoMag_Mini\Code\ErgoMag\build\generated\pico_base/pico/version.h \
+ C:\VSARM\sdk\pico\pico-sdk\src\common\pico_base\include/pico/config.h \
+ L:\Work_and_Homework\Work\Personal\ErgoMag\ErgoMag_Mini\Code\ErgoMag\build\generated\pico_base/pico/config_autogen.h \
+ C:/VSARM/sdk/pico/pico-sdk/src/boards/include/boards/pico.h \
+ C:/VSARM/sdk/pico/pico-sdk/src/rp2_common/cmsis/include/cmsis/rename_exceptions.h \
+ C:\VSARM\sdk\pico\pico-sdk\src\rp2_common\pico_platform\include/pico/platform.h \
+ C:\VSARM\sdk\pico\pico-sdk\src\rp2040\hardware_regs\include/hardware/platform_defs.h \
+ C:\VSARM\sdk\pico\pico-sdk\src\rp2040\hardware_regs\include/hardware/regs/addressmap.h \
+ C:\VSARM\sdk\pico\pico-sdk\src\rp2040\hardware_regs\include/hardware/regs/sio.h \
+ C:/VSARM/armcc/13C7F1~1.2RE/arm-none-eabi/include/sys/cdefs.h \
+ C:\VSARM\sdk\pico\pico-sdk\src\common\pico_base\include/pico/types.h \
+ C:\VSARM\sdk\pico\pico-sdk\src\common\pico_base\include/pico/error.h \
+ C:\VSARM\sdk\pico\pico-sdk\src\rp2_common\boot_stage2\include/boot_stage2/config.h
--- Code/ErgoMag/main.c
+++ Code/ErgoMag/main.c
@@ -1,7 +1,7 @@
 #include "pico/stdlib.h"
 
 int main() {
-   const uint LED_PIN = 16;
+   const uint LED_PIN = 2;
 
    gpio_init(LED_PIN);
    gpio_set_dir(LED_PIN, GPIO_OUT);
@@ -9,11 +9,10 @@ int main() {
 
 
    while (true) {
-      gpio_put(LED_PIN, 1);
-      sleep_ms(500);
-      gpio_put(LED_PIN, 0);
-      sleep_ms(500);
+      gpio_put(LED_PIN,true); 
+      sleep_ms(1000);
+      gpio_put(LED_PIN, false);
+      sleep_ms(1000);
    }
    
 }
-
